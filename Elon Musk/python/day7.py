b202881ad947d0ed80bf387a05a7a5138f1a5d1d
Anshuman Atrey <anshumanatrey@gmail.com>
2024-10-04 11:30:20 +0530

    Folder Structuring

---
+++ Elon Musk/python/day7.py
@@ -0,0 +1,14 @@
+from math import pi
+
+a=int(input("Enter First numbers: "))
+b=int(input("Enter Second numbers: "))
+print("Sum = ", a+b)
+print("Product = ", a*b)
+
+length=int(input("Enter the length of the rectangle: "))
+breadth=int(input("Enter the breadth of the rectangle: "))
+print("Area of the rectangle is: ", length*breadth)
+print("Perimeter of the rectangle is: ", 2*(length+breadth))
+
+radius=int(input("Enter the radius of the circle: "))
+print("Area of the circle is: ", pi*radius*radius)
